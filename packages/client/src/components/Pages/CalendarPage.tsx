fc6a4bcdfec43926c79dfbe55ddd93449d15198f
Quentin GARCIA <quentingarcia40@gmail.com>
2022-12-21 12:09:16 +0100

    client init with empty pages

---
+++ packages/client/src/components/Pages/CalendarPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function CalendarPage({}: Props) {
+  return (
+    <div>CalendarPage</div>
+  )
+}
+
+export default CalendarPage
---
+++ packages/client/src/components/Pages/UserProfilePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function UserProfilePage({}: Props) {
+  return (
+    <div>UserProfilePage</div>
+  )
+}
+
+export default UserProfilePage
