7aa593ba054fa3b4ca96507fbd5fcae9b8d29899
Quentin GARCIA <quentingarcia40@gmail.com>
2023-01-08 15:02:58 +0100

    add secondary mode to button

--- packages/client/src/components/Atoms/Button.tsx
+++ packages/client/src/components/Atoms/Button.tsx
@@ -3,27 +3,57 @@ import styled from 'styled-components';
 
 export type ButtonProps = React.HtmlHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
+  mode?: 'primary' | 'secondary';
 };
 
-function Button({ children, ...props }: ButtonProps) {
-  return <StyledButton {...props}>{children}</StyledButton>;
+function Button({ children, mode, ...props }: ButtonProps) {
+  const renderButton = (props: ButtonProps) => {
+    switch (mode) {
+      case 'primary':
+        return <PrimaryButton {...props} />;
+      case 'secondary':
+        return <SecondaryButton {...props} />;
+      default:
+        return <PrimaryButton {...props} />;
+    }
+  };
+
+  return renderButton({ children, ...props } as ButtonProps);
 }
 
 export default Button;
 
 const StyledButton = styled.button`
-  background-color: #000;
-  color: #fff;
   border: none;
-  border-radius: 4px;
-  padding: 8px 16px;
-  font-size: 16px;
+  border-radius: 8px;
+  padding: 12px 20px;
+  font-size: 12px;
+`;
+
+const PrimaryButton = styled(StyledButton)`
+  background-color: #3082ff;
+  color: #fff;
 
   &:hover {
-    background-color: #333;
+    background-color: #5b9cff;
   }
 
   &:active {
-    background-color: #666;
+    background-color: #7eb1fe;
+  }
+`;
+
+const SecondaryButton = styled(StyledButton)`
+  background-color: #fff;
+  color: #8a9197;
+  font-weight: bold;
+  //add a slight shadow to the button
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.1);
+  &:hover {
+    background-color: #5b9cff;
+  }
+
+  &:active {
+    background-color: #7eb1fe;
   }
 `;
--- packages/storybook/src/stories/Button.stories.tsx
+++ packages/storybook/src/stories/Button.stories.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import Button, {ButtonProps} from '../../../client/src/components/Atoms/Button';
+import Button, {
+  ButtonProps,
+} from '../../../client/src/components/Atoms/Button';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 export default {
-    title: 'Button',
-    component: Button,
+  title: 'Button',
+  component: Button,
 } as Meta;
-
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
-    children: 'Button',
-    onClick: () => console.log('clicked'),
+  children: 'Button',
+  onClick: () => console.log('clicked'),
 };
 
-
-
-
-
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Button',
+  onClick: () => console.log('clicked'),
+  mode: 'secondary',
+};
